b3a1719a175791ed8737f5ac4ec8a9931c60873c
AniruddhaAyan23 <7.aniruddha@gmail.com>
2025-12-22 18:58:11 +0600

    toasteer and functionalities are set

--- src/App.jsx
+++ src/App.jsx
@@ -25,3 +25,87 @@ import AllRequests from './pages/hr/AllRequests';
 import EmployeeList from './pages/hr/EmployeeList';
 import UpgradePackage from './pages/hr/UpgradePackage';
 import HRProfile from './pages/hr/Profile';
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          
+          <main className="flex-grow">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register-employee" element={<RegisterEmployee />} />
+              <Route path="/register-hr" element={<RegisterHR />} />
+
+              {/* Employee Routes */}
+              <Route path="/employee/my-assets" element={
+                <PrivateRoute role="employee">
+                  <MyAssets />
+                </PrivateRoute>
+              } />
+              <Route path="/employee/request-asset" element={
+                <PrivateRoute role="employee">
+                  <RequestAsset />
+                </PrivateRoute>
+              } />
+              <Route path="/employee/my-team" element={
+                <PrivateRoute role="employee">
+                  <MyTeam />
+                </PrivateRoute>
+              } />
+              <Route path="/employee/profile" element={
+                <PrivateRoute role="employee">
+                  <EmployeeProfile />
+                </PrivateRoute>
+              } />
+
+              {/* HR Routes */}
+              <Route path="/hr/assets" element={
+                <HRRoute>
+                  <AssetList />
+                </HRRoute>
+              } />
+              <Route path="/hr/add-asset" element={
+                <HRRoute>
+                  <AddAsset />
+                </HRRoute>
+              } />
+              <Route path="/hr/requests" element={
+                <HRRoute>
+                  <AllRequests />
+                </HRRoute>
+              } />
+              <Route path="/hr/employees" element={
+                <HRRoute>
+                  <EmployeeList />
+                </HRRoute>
+              } />
+              <Route path="/hr/upgrade" element={
+                <HRRoute>
+                  <UpgradePackage />
+                </HRRoute>
+              } />
+              <Route path="/hr/profile" element={
+                <HRRoute>
+                  <HRProfile />
+                </HRRoute>
+              } />
+
+              {/* 404 */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
